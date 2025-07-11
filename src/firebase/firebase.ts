dd6945967b69d9170f3b1905ddc53dc1870ea2a6
Ose John Oziegbe <jiade1233@gmail.com>
2025-07-11 19:07:47 +0100

    fixed secret exposure

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,15 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCdhQtW5j2Nidrw1Gzjdfv2svNyOnzbNqY",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	authDomain: "pennywise-1271f.firebaseapp.com",
 	projectId: "pennywise-1271f",
 	storageBucket: "pennywise-1271f.firebasestorage.app",
 	messagingSenderId: "809034201893",
-	appId: "1:809034201893:web:92c5a5bee2b7177bd0b8cc",
-	measurementId: "G-51PVYK3CLQ",
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyCdhQtW5j2Nidrw1Gzjdfv2svNyOnzbNqY",
+// 	authDomain: "pennywise-1271f.firebaseapp.com",
+// 	projectId: "pennywise-1271f",
+// 	storageBucket: "pennywise-1271f.firebasestorage.app",
+// 	messagingSenderId: "809034201893",
+// 	appId: "1:809034201893:web:92c5a5bee2b7177bd0b8cc",
+// 	measurementId: "G-51PVYK3CLQ",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+console.log(analytics);
