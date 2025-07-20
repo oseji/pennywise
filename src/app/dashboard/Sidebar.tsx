a6733b0d06f691146c79ff06a0b7ca1e3977f62d
Ose John Oziegbe <jiade1233@gmail.com>
2025-07-20 20:02:50 +0100

    worked on active sidebar functionality

--- src/app/dashboard/Sidebar.tsx
+++ src/app/dashboard/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "@/firebase/firebase";
 import { signOut } from "firebase/auth";
@@ -21,6 +21,28 @@ const Sidebar = () => {
 	const router = useRouter();
 
 	const [errorMessage, setErrorMessage] = useState<string>("");
+
+	const sideBarItems = useRef<(HTMLSpanElement | null)[]>([]);
+	const sideBarTexts = useRef<(HTMLSpanElement | null)[]>([]);
+
+	const toggleActiveSidebaritem = (index: number) => {
+		sideBarItems.current.forEach((item, itemNumber) => {
+			if (index === itemNumber) {
+				item?.classList.add("sidebarIndicatorActive");
+			} else {
+				item?.classList.remove("sidebarIndicatorActive");
+				item?.classList.add("sidebarIndicator");
+			}
+		});
+
+		sideBarTexts.current.forEach((item, itemNumber) => {
+			if (index === itemNumber) {
+				item?.classList.add("activeSidebarText");
+			} else {
+				item?.classList.remove("activeSidebarText");
+			}
+		});
+	};
 
 	const formatLogoutError = (error: unknown): string => {
 		if (error instanceof FirebaseError) {
@@ -54,8 +76,8 @@ const Sidebar = () => {
 		}
 	};
 	return (
-		<div className=" p-6 w-64">
-			<div className=" flex flex-row items-center justify-center gap-4 font-bold text-2xl mb-4 border-b border-slate-400 pb-4 px-6">
+		<div className="w-64 p-6 ">
+			<div className="flex flex-row items-center justify-center gap-4 px-6 pb-4 mb-4 text-2xl font-bold border-b border-slate-400">
 				<span className=" rounded-full bg-[#B7E4C7] text-[#40916C] px-4 py-2">
 					P
 				</span>
@@ -63,65 +85,170 @@ const Sidebar = () => {
 				<span>Pennywise</span>
 			</div>
 
-			<div className=" flex flex-col gap-10 items-start">
-				<div className=" flex flex-col gap-4">
-					<h1 className=" text-slate-400 mb-2">Overview</h1>
+			<div className="flex flex-col items-start gap-10 ">
+				<div className="flex flex-col w-full gap-4">
+					<h1 className="mb-2 text-slate-400">Overview</h1>
 
 					<Link href={"/dashboard/overview"}>
-						<div className=" sidebarRow">
-							<span className=" sidebarIndicatorActive"></span>
+						<div
+							className=" sidebarRow"
+							onClick={() => toggleActiveSidebaritem(0)}
+						>
+							<span
+								className=" sidebarIndicatorActive"
+								ref={(el) => {
+									sideBarItems.current[0] = el;
+								}}
+							></span>
 							<Image src={dashboardIcon} alt="Dashboard" />
-							<span>Dashboard</span>
+							<span
+								className=" sidebarText activeSidebarText"
+								ref={(el) => {
+									sideBarTexts.current[0] = el;
+								}}
+							>
+								Dashboard
+							</span>
 						</div>
 					</Link>
 
 					<Link href={"/dashboard/income"}>
-						<div className=" sidebarRow">
-							<span className=" sidebarIndicator"></span>
+						<div
+							className=" sidebarRow"
+							onClick={() => toggleActiveSidebaritem(1)}
+						>
+							<span
+								className=" sidebarIndicator"
+								ref={(el) => {
+									sideBarItems.current[1] = el;
+								}}
+							></span>
 							<Image src={incomeIcon} alt="income" />
-							<span>income</span>
+							<span
+								className=" sidebarText"
+								ref={(el) => {
+									sideBarTexts.current[1] = el;
+								}}
+							>
+								income
+							</span>
 						</div>
 					</Link>
 
 					<Link href={"/dashboard/budget"}>
-						<div className=" sidebarRow">
-							<span className=" sidebarIndicator"></span>
+						<div
+							className=" sidebarRow"
+							onClick={() => toggleActiveSidebaritem(2)}
+						>
+							<span
+								className=" sidebarIndicator"
+								ref={(el) => {
+									sideBarItems.current[2] = el;
+								}}
+							></span>
 							<Image src={budgetIcon} alt="budget" />
-							<span>budget</span>
+							<span
+								className=" sidebarText"
+								ref={(el) => {
+									sideBarTexts.current[2] = el;
+								}}
+							>
+								budget
+							</span>
 						</div>
 					</Link>
 
 					<Link href={"/dashboard/expenses"}>
-						<div className=" sidebarRow">
-							<span className=" sidebarIndicator"></span>
+						<div
+							className=" sidebarRow"
+							onClick={() => toggleActiveSidebaritem(3)}
+						>
+							<span
+								className=" sidebarIndicator"
+								ref={(el) => {
+									sideBarItems.current[3] = el;
+								}}
+							></span>
 							<Image src={expensesIcon} alt="expenses" />
-							<span>expenses</span>
+							<span
+								className=" sidebarText"
+								ref={(el) => {
+									sideBarTexts.current[3] = el;
+								}}
+							>
+								expenses
+							</span>
 						</div>
 					</Link>
 
 					<Link href={"/dashboard/savings"}>
-						<div className=" sidebarRow">
-							<span className=" sidebarIndicator"></span>
+						<div
+							className=" sidebarRow"
+							onClick={() => toggleActiveSidebaritem(4)}
+						>
+							<span
+								className=" sidebarIndicator"
+								ref={(el) => {
+									sideBarItems.current[4] = el;
+								}}
+							></span>
 							<Image src={savingsIcon} alt="savings" />
-							<span>savings</span>
+							<span
+								className=" sidebarText"
+								ref={(el) => {
+									sideBarTexts.current[4] = el;
+								}}
+							>
+								savings
+							</span>
 						</div>
 					</Link>
 
-					<div className=" sidebarRow">
-						<span className=" sidebarIndicator"></span>
+					<div
+						className=" sidebarRow"
+						onClick={() => toggleActiveSidebaritem(5)}
+					>
+						<span
+							className=" sidebarIndicator"
+							ref={(el) => {
+								sideBarItems.current[5] = el;
+							}}
+						></span>
 						<Image src={profileIcon} alt="profile" />
-						<span>profile</span>
+						<span
+							className=" sidebarText"
+							ref={(el) => {
+								sideBarTexts.current[5] = el;
+							}}
+						>
+							profile
+						</span>
 					</div>
 
-					<div className=" sidebarRow">
-						<span className=" sidebarIndicator"></span>
+					<div
+						className=" sidebarRow"
+						onClick={() => toggleActiveSidebaritem(6)}
+					>
+						<span
+							className=" sidebarIndicator"
+							ref={(el) => {
+								sideBarItems.current[6] = el;
+							}}
+						></span>
 						<Image src={historyIcon} alt="history" />
-						<span>history</span>
+						<span
+							className=" sidebarText"
+							ref={(el) => {
+								sideBarTexts.current[6] = el;
+							}}
+						>
+							history
+						</span>
 					</div>
 				</div>
 
-				<div className=" flex flex-col gap-4">
-					<h1 className=" text-slate-400 mb-2">Others</h1>
+				<div className="flex flex-col gap-4 ">
+					<h1 className="mb-2 text-slate-400">Others</h1>
 
 					<div className=" sidebarRow">
 						<Image src={settingsIcon} alt="settings" />
